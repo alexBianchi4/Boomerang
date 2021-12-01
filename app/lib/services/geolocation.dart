71665002b7f371608ca8b41ce5947b6f14c68f55
Will-Dolan-VI <78272577+Will-Dolan-VI@users.noreply.github.com>
2021-12-01 12:20:05 -0500

    Put "requestPermission" in it's own method and added getDistance method

--- app/lib/services/geolocation.dart
+++ app/lib/services/geolocation.dart
@@ -3,12 +3,20 @@ import 'package:app/services/auth.dart';
 import 'package:geolocator/geolocator.dart';
 
 class GeolocationService {
-  Future<Position?> getPosition() async{
+
+  //Gets permission from user to get location
+  Future<LocationPermission> requestPermission() async{
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied || permission == LocationPermission.deniedForever)
     {
       permission = await Geolocator.requestPermission();
     }
+    return permission;
+  }
+
+  //Gets permission from user and finds current location
+  Future<Position?> getPosition() async{
+    LocationPermission permission = await requestPermission();
 
     if (permission != LocationPermission.denied && permission != LocationPermission.deniedForever)
     {
@@ -19,4 +27,19 @@ class GeolocationService {
       return null;
     }
   }
+
+  //Finds distance between passed lat/long and current position
+  Future<double> getDistance(double latitude, double longitude) async{
+    LocationPermission permission = await requestPermission();
+
+    if (permission != LocationPermission.denied && permission != LocationPermission.deniedForever)
+    {
+      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      return Geolocator.distanceBetween(latitude, longitude, position.latitude, position.longitude);
+    }
+    else{
+      return 0;
+    }
+  }
+
 }
