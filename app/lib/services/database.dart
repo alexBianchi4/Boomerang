cea285eacf628cf2cc8c6b651ae453cd72161faf
Soman <somansheikh786@gmail.com>
2021-12-05 20:21:14 -0500

    added search functionality to the results page appbar, changed loading circle to center of screen

--- app/lib/services/database.dart
+++ app/lib/services/database.dart
@@ -52,9 +52,8 @@ class DatabaseService {
     // get the id of the document that was just created in listing
     String id = docref.id;
 
-    await listingsCollection
-      .doc(docref.id)
-      .update({'postId': id,
+    await listingsCollection.doc(docref.id).update({
+      'postId': id,
     });
 
     // use that id as the name for the image we are putting in firestore
